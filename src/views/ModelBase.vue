fc80cd459576304d22909115ba3414b25b9ef82d
zhengkun <1615966554@qq.com>
2021-04-06 16:12:40 +0800

    Initial commit

---
+++ src/views/ModelBase.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>页面一</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "PageOne"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/SystemLog.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "SystemLog"
+    }
+</script>
+
+<style scoped>
+
+</style>
